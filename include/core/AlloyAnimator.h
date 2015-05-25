950357853f932ec0e9b68ea44f2b0b9794dc6e31
bclucas <blake.c.lucas@gmail.com>
2015-05-25 00:58:41 -0700

    Virtualized operator

--- include/core/AlloyAnimator.h
+++ include/core/AlloyAnimator.h
@@ -12,17 +12,19 @@
 #include <list>
 namespace aly{
 struct Interpolant{
+	virtual double operator()(double t) const {return 0;}
+	virtual inline ~Interpolant(){};
 };
 struct Linear : public Interpolant
 {
-	inline double operator()(double t) const {
+	virtual inline double operator()(double t) const {
 	    return t;
     }
 };
 
 struct Smoothstep : public Interpolant
 {
-    inline double operator()(double t) const {
+	virtual inline double operator()(double t) const {
 	    auto scale = t * t * (3.f - 2.f * t);
 	    return scale * 1.0f;
     }
@@ -30,45 +32,45 @@ struct Smoothstep : public Interpolant
 
 struct SineIn : public Interpolant
 {
-    inline double operator()(double t) const {
+	virtual inline double operator()(double t) const {
 	    return 1.0f - std::cos(t * ALY_PI / 2.f);
     }
 };
 
 struct SineOut : public Interpolant
 {
-    inline double operator()(double t) const {
+	virtual inline double operator()(double t) const {
 	    return (t < 0.5f) ? (1.0f - std::cos(t * ALY_PI)) * 0.5f : 1.0f - 1.0f - std::cos((1.0f-t) * ALY_PI) * 0.5f;
     }
 };
 
 struct ExponentialIn: public Interpolant
 {
-    inline double operator()(double t) const {
+	virtual inline double operator()(double t) const {
 	    return powf(2.0f, 10.f * (t - 1.0f));
     }
  };
 struct ExponentialOut: public Interpolant
 {
-	inline double operator()(double t) const {
+	virtual inline double operator()(double t) const {
  	    return (t < 0.5f) ? std::pow(2.0f, 10.f * (2.0f*t - 1.0f)) * 0.5f : 1.0f - std::pow(2.0f, 10.f * (1.0f-2.0f*t)) * 0.5f;
     }
 };
 struct QuadraticIn: public Interpolant
 {
-    inline double operator()(double t) const {
+	virtual inline double operator()(double t) const {
 	    return t * t;
     }
 };
 struct QuadraticOut: public Interpolant
 {
-    inline double operator()(double t) const {
+	virtual inline double operator()(double t) const {
         return (t < 0.5f) ? (t*t * 2.0f) * 0.5f : 1.0f - (2.0f - t*t * 2.0f) * 0.5f;
     }
 };
 struct CubicPulse : public Interpolant
 {
-    inline double operator()(double t) const {
+	virtual inline double operator()(double t) const {
         double c = 1.0, w = 0.9f;
 	    double x = std::abs(t - c);
 	    if (x > w) return 0.0f;
@@ -79,9 +81,9 @@ struct CubicPulse : public Interpolant
 
 struct Tween{
 	std::shared_ptr<Tweenable> object;
-	Interpolant interp;
+	Interpolant interpolant;
 	double duration;
-	Tween(const std::shared_ptr<Tweenable>& object,const Interpolant& interp,double duration):object(object),interp(interp),duration(duration){
+	Tween(const std::shared_ptr<Tweenable>& object,const Interpolant& interpolant,double duration):object(object),interpolant(interpolant),duration(duration){
 	}
 };
 class Animator{
