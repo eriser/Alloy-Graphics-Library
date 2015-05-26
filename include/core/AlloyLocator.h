a88a7f5c920d2e74968b421977d798193cc82bc2
bclucas <blake.c.lucas@gmail.com>
2015-05-25 20:44:41 -0700

    Added more headers

--- include/core/AlloyLocator.h
+++ include/core/AlloyLocator.h
@@ -18,41 +18,11 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#include "AlloyAnimator.h"
-namespace aly {
-std::shared_ptr<Tween>& Animator::add(const std::shared_ptr<Tween>& tween) {
-	tweens[parity].push_back(tween);
-	return tweens[parity].back();
-}
-void Animator::reset() {
-	for (std::shared_ptr<Tween>& tween : tweens[parity]) {
-		tween->reset();
-	}
-}
-bool Animator::step(double dt) {
-	std::list<std::shared_ptr<Tween>>& current = tweens[parity];
-	std::list<std::shared_ptr<Tween>>& next = tweens[1 - parity];
-	if (current.size() == 0)
-		return false;
-	for (std::shared_ptr<Tween>& tween : current) {
-		if (tween->step(dt)) {
-			next.push_back(tween);
-		} else {
-			finished.push_back(tween);
-		}
-	}
-	current.clear();
-	parity = 1 - parity;
-	return true;
-}
-bool Animator::firePostEvents() {
-	if (finished.size() == 0)
-		return false;
-	for (std::shared_ptr<Tween> tween : finished) {
-		tween->firePostEvents();
-	}
-	finished.clear();
-	return true;
-}
-}
 
+#ifndef ALLOYLOCATOR_H_
+#define ALLOYLOCATOR_H_
+#include "AlloyMath.h"
+namespace aly{
+
+}
+#endif /* ALLOYLOCATOR_H_ */
