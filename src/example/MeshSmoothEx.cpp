dd20525c6311a860fdd5f4e772cc94ce6479e81a
bclucas <blake.c.lucas@gmail.com>
2015-10-14 20:33:09 -0700

    Simplification

--- src/example/MeshSmoothEx.cpp
+++ src/example/MeshSmoothEx.cpp
@@ -23,19 +23,22 @@
 #include "../../include/example/MeshSmoothEx.h"
 using namespace aly;
 MeshSmoothEx::MeshSmoothEx() :
-		Application(800,600, "Mesh Smoothing Example"),matcapShader(getFullPath("images/JG_Gold.png")) {
+		Application(800, 600, "Mesh Smoothing Example"), matcapShader(
+				getFullPath("images/JG_Gold.png")) {
 }
 bool MeshSmoothEx::init(Composite& rootNode) {
-	box3f renderBBox = box3f(float3(-0.5f, -0.5f, -0.5f),float3(1.0f, 1.0f, 1.0f));
+	box3f renderBBox = box3f(float3(-0.5f, -0.5f, -0.5f),
+			float3(1.0f, 1.0f, 1.0f));
 	mesh.load(getFullPath("models/icosahedron.ply"));
 	std::srand(812731);
-	for (int c = 0;c < 5;c++) {
+	for (int c = 0; c < 5; c++) {
 		Subdivide(mesh, SubDivisionScheme::Loop);
 		mesh.updateVertexNormals();
-		int N = (int)mesh.vertexLocations.size();
-		for (int n = 0;n < N;n++) {
+		int N = (int) mesh.vertexLocations.size();
+		for (int n = 0; n < N; n++) {
 			float3 norm = -mesh.vertexNormals[n];
-			mesh.vertexLocations[n] += 0.6f*norm*((rand() % 4096) / 4096.0f-0.3f)/(float)(1<<c);
+			mesh.vertexLocations[n] += 0.6f * norm
+					* ((rand() % 4096) / 4096.0f - 0.3f) / (float) (1 << c);
 		}
 	}
 	Subdivide(mesh, SubDivisionScheme::Loop);
@@ -44,9 +47,11 @@ bool MeshSmoothEx::init(Composite& rootNode) {
 	int w = getContext()->width();
 	int h = getContext()->height();
 	//Make region on screen to render 3d view
-	renderRegion=MakeRegion("Render View",CoordPerPX(0.5,0.5,-w/2,-h/2),CoordPX(w,h),COLOR_NONE,COLOR_WHITE,UnitPX(1.0f));
+	renderRegion = MakeRegion("Render View",
+			CoordPerPX(0.5, 0.5, -w / 2, -h / 2), CoordPX(w, h), COLOR_NONE,
+			COLOR_WHITE, UnitPX(1.0f));
 	//Initialize depth buffer to store the render
-	depthFrameBuffer.initialize(w,h);
+	depthFrameBuffer.initialize(w, h);
 	//Set up camera
 	camera.setNearFarPlanes(0.1f, 2.0f);
 	camera.setZoom(0.75f);
@@ -57,14 +62,13 @@ bool MeshSmoothEx::init(Composite& rootNode) {
 	//Add listener to respond to mouse manipulations
 	addListener(&camera);
 	textLabel = TextLabelPtr(
-		new TextLabel("",
-			CoordPX(5,5), CoordPercent(0.5f, 0.5f)));
+			new TextLabel("", CoordPX(5, 5), CoordPercent(0.5f, 0.5f)));
 	textLabel->fontSize = UnitPX(20.0f);
 	textLabel->fontType = FontType::Bold;
 	textLabel->fontStyle = FontStyle::Outline;
 	TextButtonPtr smoothButton = TextButtonPtr(
-		new TextButton("Smooth", CoordPerPX(0.5f,1.0f, -50.0f,-35.0f),
-			CoordPX(100, 30)));
+			new TextButton("Smooth", CoordPerPX(0.5f, 1.0f, -50.0f, -35.0f),
+					CoordPX(100, 30)));
 	rootNode.add(renderRegion);
 	rootNode.add(textLabel);
 	rootNode.add(smoothButton);
@@ -72,18 +76,17 @@ bool MeshSmoothEx::init(Composite& rootNode) {
 		textLabel->label = "Smoothing Mesh ...";
 		smoothButton->setVisible(false);
 		smooth();
-		getContext()->addDeferredTask([=]() {
-			mesh.setDirty(true);
-			camera.setDirty(true);
-			smoothButton->setVisible(true);
-			textLabel->label = "";
-		});
+		mesh.setDirty(true);
+		camera.setDirty(true);
+		smoothButton->setVisible(true);
+		textLabel->label = "";
 	}));
-	smoothButton->onMouseDown = [=](AlloyContext* context, const InputEvent& e) {
-		workerTask->cancel();
-		workerTask->execute();
-		return true;
-	};
+	smoothButton->onMouseDown =
+			[=](AlloyContext* context, const InputEvent& e) {
+				workerTask->cancel();
+				workerTask->execute();
+				return true;
+			};
 	return true;
 }
 void MeshSmoothEx::smooth() {
@@ -93,19 +96,17 @@ void MeshSmoothEx::smooth() {
 	std::vector<float> angles;
 	std::vector<float> weights;
 	float smoothness = 10.0f;
-	int N = (int)mesh.vertexLocations.size();
-	SparseMatrix1f A(N,N);
+	int N = (int) mesh.vertexLocations.size();
+	SparseMatrix1f A(N, N);
 	Vector3f b(N);
-	for (std::list<uint32_t>& nbrs : nbrTable)
-	{
-		int K = (int)nbrs.size() - 1;
+	for (std::list<uint32_t>& nbrs : nbrTable) {
+		int K = (int) nbrs.size() - 1;
 		float3 pt = mesh.vertexLocations[index];
 		angles.resize(K);
 		weights.resize(K);
 		{
 			auto nbrIter = nbrs.begin();
-			for (int k = 0; k < K; k++)
-			{
+			for (int k = 0; k < K; k++) {
 				float3 current = mesh.vertexLocations[*nbrIter];
 				nbrIter++;
 				float3 next = mesh.vertexLocations[*nbrIter];
@@ -116,10 +117,10 @@ void MeshSmoothEx::smooth() {
 		{
 			auto nbrIter = nbrs.begin();
 			nbrIter++;
-			for (int k = 0; k < K; k++)
-			{
+			for (int k = 0; k < K; k++) {
 				float3 ptNext = mesh.vertexLocations[*nbrIter];
-				float w = (angles[k] + angles[(k + 1)%K]) / distance(pt, ptNext);
+				float w = (angles[k] + angles[(k + 1) % K])
+						/ distance(pt, ptNext);
 				wsum += w;
 				weights[k] = w;
 				nbrIter++;
@@ -128,29 +129,30 @@ void MeshSmoothEx::smooth() {
 		{
 			auto nbrIter = nbrs.begin();
 			nbrIter++;
-			for (int k = 0; k < K; k++)
-			{
+			for (int k = 0; k < K; k++) {
 				float w = -smoothness * weights[k] / wsum;
 				A.set(index, *nbrIter, w);
 				nbrIter++;
 			}
 		}
-		A.set(index, index, smoothness+1);
+		A.set(index, index, smoothness + 1);
 		b[index] = pt;
 		index++;
 	}
-	SolveBICGStab(b, A, mesh.vertexLocations, 100, 1E-6f, [this](int iter,double  error) {
-		textLabel->label = MakeString() << "Smooth [" << iter << "] Error: " << error;
-	});
+	SolveBICGStab(b, A, mesh.vertexLocations, 100, 1E-6f,
+			[this](int iter,double error) {
+				textLabel->label = MakeString() << "Smooth [" << iter << "] Error: " << error;
+			});
 	mesh.updateVertexNormals();
 }
-void MeshSmoothEx::draw(AlloyContext* context){
+void MeshSmoothEx::draw(AlloyContext* context) {
 	if (camera.isDirty()) {
 		//Compute depth and normals only when camera view changes.
 		depthAndNormalShader.draw(mesh, camera, depthFrameBuffer);
 	}
 	//Recompute lighting at every draw pass.
-	matcapShader.draw(depthFrameBuffer.getTexture(),camera,renderRegion->getBounds(),context->getViewport(),RGBAf(1.0f));
+	matcapShader.draw(depthFrameBuffer.getTexture(), camera,
+			renderRegion->getBounds(), context->getViewport(), RGBAf(1.0f));
 	camera.setDirty(false);
 }
 
