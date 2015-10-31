86dd5a55b52f01c346c6452f2e7f00c2e6565da2
bclucas <blake.c.lucas@gmail.com>
2015-10-31 13:17:04 -0700

    Changed min gl version

--- src/core/AlloyContext.cpp
+++ src/core/AlloyContext.cpp
@@ -319,7 +319,7 @@ namespace aly {
 		glfwSetErrorCallback(
 			[](int error, const char* desc) {std::cout << "GLFW Error [" << error << "] " << desc << std::endl;});
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, 1);
@@ -347,7 +347,7 @@ namespace aly {
 		viewSize = int2(width, height);
 
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, 1);
